155acd31fb1da15f8c2ebb225db184a57b4785bc
lorlorv <dev.eunseo@gmail.com>
2023-01-04 22:30:55 +0900

    [Chore] add branch dev

---
+++ waither/waither/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  waither
+//
+//  Created by eunseo on 2023/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
