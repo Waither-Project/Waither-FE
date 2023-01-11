4e26930cd71991845229f474075fccce63fdbd79
theunghee02 <20211150@sungshin.ac.kr>
2023-01-11 16:28:29 +0900

    [Docs] add MainPage storyboard, ViewController Main에 넣으면 출동날 듯하여 MainPage 따로 만들어 메인 기능 구현

---
+++ waither/waither/MainPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageViewController.swift
+//  waither
+//
+//  Created by 최승희 on 2023/01/11.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
