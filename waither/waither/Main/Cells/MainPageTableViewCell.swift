c0b926f6c88b32ec0998a070b157735f3b1a9240
theunghee02 <20211150@sungshin.ac.kr>
2023-01-11 20:56:31 +0900

    [Docs] add MainPageCell's storyboard, ViewController rightarrow 이미지 깃 문제로 이번 커밋에 미포함

---
+++ waither/waither/Main/Cells/MainPageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainPageTableViewCell.swift
+//  waither
+//
+//  Created by 최승희 on 2023/01/11.
+//
+
+import UIKit
+
+class MainPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
