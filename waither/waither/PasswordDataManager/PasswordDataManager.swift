b4921085ee6a74c9ad01b60803334fc0dba254c2
김유빈 <yubin6894@gmail.com>
2023-01-18 18:32:22 +0900

    [Docs] PasswordDataManager folder

---
+++ waither/waither/PasswordDataManager/PasswordDataManager.swift
@@ -0,0 +1,25 @@
+//
+//  PasswordDataManager.swift
+//  waither
+//
+//  Created by 김유빈 on 2023/01/18.
+//
+
+import Alamofire
+
+class PasswordDataManager {
+    func passwordDataManager() {
+        AF.request("http://www.waither.shop/",
+                   method: .get,
+                   parameters: nil)
+        .validate()
+        .responseDecodable(of: PasswordModel.self) { response in
+            switch response.result {
+            case .success(let result):
+                print("성공")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
