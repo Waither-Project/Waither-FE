5c1fb69d26d56a8d082fa1b227e31de68aef728a
lorlorv <dev.eunseo@gmail.com>
2023-01-21 20:37:19 +0900

    [Feat] LoginByEmail UI

--- waither/waither/LoginViewController.swift
+++ waither/waither/LoginViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginByEmailLabel: UIButton!
+    @IBOutlet weak var signinByEmailBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loginLabel.addCharacterSpacing()
+        
+//        loginByEmailLabel.addTextSpacing(-1)
+//        loginByEmailLabel.titleLabel?.font =
+        
+        signinByEmailBtn.layer.borderWidth = 1
+        signinByEmailBtn.layer.borderColor = UIColor.main_blue?.cgColor
+        signinByEmailBtn.layer.cornerRadius = 20
     }
     
-
+    @IBAction func loginByEmailBtnClicked(_ sender: Any) {
+        guard let loginByEmailVC = self.storyboard?.instantiateViewController(identifier: "LoginByEmailViewController")
+        else{
+                return
+            }
+        self.navigationController?.pushViewController(loginByEmailVC, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,3 +44,21 @@ class LoginViewController: UIViewController {
     */
 
 }
+
+extension UIColor{
+    static let main_blue = UIColor(named: "Splash_blue")
+    static let none_gray = UIColor(named: "Gray")
+}
+
+extension UIButton{
+
+    func addTextSpacing(_ letterSpacing: CGFloat){
+        let attributedString = NSMutableAttributedString(string: (self.titleLabel?.text!)!)
+        attributedString.addAttribute(NSAttributedString.Key.kern, value: letterSpacing, range: NSRange(location: 0, length: (self.titleLabel?.text!.count)!))
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+
+}
+
+
+
