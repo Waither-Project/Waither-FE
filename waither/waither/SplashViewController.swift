6a570c591cc675d47f591b8e24e091196ecbc2c0
lorlorv <dev.eunseo@gmail.com>
2023-01-24 01:51:26 +0900

    [Fix] connect storyboard

--- waither/waither/SplashViewController.swift
+++ waither/waither/SplashViewController.swift
@@ -13,25 +13,20 @@ class SplashViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.alpha = 0
-        UIView.animate(withDuration: 1.0, delay: 1.5, options: .curveEaseOut
-                       , animations: { self.view.alpha = 1 },
-                       completion: { finished in let Storyboard = UIStoryboard.init(name: "Main", bundle: nil) //splash 종료 후 Login 화면으로 이동
-                                        guard let VC = Storyboard.instantiateViewController(identifier: "Login") as? ViewController else { return }
-                                        self.present(VC, animated: false, completion: nil)
+        UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveEaseOut,
+                       animations: { self.view.alpha = 1 },
+                       completion: {
+                            finished in let Storyboard = UIStoryboard.init(name: "Login", bundle: nil) //splash 종료 후 Login 화면으로 이동
+                        
+                            let nextVC = Storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+                            let navController = UINavigationController(rootViewController: nextVC)
+                            navController.modalTransitionStyle = .coverVertical
+                            navController.modalPresentationStyle = .fullScreen
+                            self.present(navController, animated:true, completion: nil)
+                        
                         }
         )
 
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
