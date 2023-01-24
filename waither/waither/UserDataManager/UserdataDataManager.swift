7db4f880a73b181616525668ae9d33f8039cd156
김유빈 <yubin6894@gmail.com>
2023-01-24 14:13:29 +0900

    [Docs] UserDataManager folder

--- waither/waither/UserDataManager/UserdataDataManager.swift
+++ waither/waither/UserDataManager/UserdataDataManager.swift
@@ -5,4 +5,23 @@
 //  Created by 김유빈 on 2023/01/24.
 //
 
-import Foundation
+import Alamofire
+
+class UserdataDataManager {
+    func userdataDataManager() {
+        AF.request("http://www.waither.shop/app/settings/user",
+                   method: .get,
+                   parameters: nil)
+        .validate()
+        .responseDecodable(of: UserdataModel.self) { response in
+            switch response.result {
+            case .success(_):
+                print("성공")
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+
--- waither/waither/UserDataManager/UserdataModel.swift
+++ waither/waither/UserDataManager/UserdataModel.swift
@@ -1,8 +1,11 @@
 //
-//  UserDataModel.swift
+//  UserdataModel.swift
 //  waither
 //
 //  Created by 김유빈 on 2023/01/24.
 //
 
-import Foundation
+struct UserdataModel : Decodable {
+    let isSuccess : Bool?
+}
+
