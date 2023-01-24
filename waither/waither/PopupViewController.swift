d4a9c9cec23e2249ebedd4eb2130b91a74cb4b96
김유빈 <yubin6894@gmail.com>
2023-01-24 13:28:00 +0900

    [Fix] Popup UI

--- waither/waither/PopupViewController.swift
+++ waither/waither/PopupViewController.swift
@@ -18,24 +18,21 @@ extension UILabel {
 }
 
 extension NSMutableAttributedString {
-    var fontSize: CGFloat {
-        return 16
-    }
-    var boldFont: UIFont {
-        return UIFont(name: "AppleSDGothicNeoB00", size: fontSize) ?? UIFont.boldSystemFont(ofSize: fontSize)
-    }
-    var normalFont: UIFont {
-        return UIFont(name: "AppleSDGothicNeoM00", size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+    func ENGbold(string: String, fontSize: CGFloat) -> NSMutableAttributedString {
+        let font = UIFont(name: "SF Pro Bold", size: fontSize)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font!]
+        self.append(NSAttributedString(string: string, attributes: attributes))
+        return self
     }
     func regular(string: String, fontSize: CGFloat) -> NSMutableAttributedString {
-            let font = UIFont.systemFont(ofSize: fontSize)
-            let attributes: [NSAttributedString.Key: Any] = [.font: font]
-            self.append(NSAttributedString(string: string, attributes: attributes))
-            return self
+        let font = UIFont(name: "Apple SD Gothic Neo Medium", size: fontSize)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font!]
+        self.append(NSAttributedString(string: string, attributes: attributes))
+        return self
     }
     func bold(string: String, fontSize: CGFloat) -> NSMutableAttributedString {
-        let font = UIFont.boldSystemFont(ofSize: fontSize)
-        let attributes: [NSAttributedString.Key: Any] = [.font: font]
+        let font = UIFont(name: "Apple SD Gothic Neo Bold", size: fontSize)
+        let attributes: [NSAttributedString.Key: Any] = [.font: font!]
         self.append(NSAttributedString(string: string, attributes: attributes))
         return self
     }
@@ -78,7 +75,7 @@ class PopupViewController: UIViewController {
         logoutLabel?.attributedText = NSMutableAttributedString()
             .regular(string: "지금 로그아웃하시면\n", fontSize: 15)
             .bold(string: "사용자 데이터를 저장할 수 없어요.\n", fontSize: 15)
-            .regular(string: "그래도 로그아웃해드릴까요?", fontSize: 15)
+            .regular(string: "그래도 로그아웃해 드릴까요?", fontSize: 15)
         
         resignLabel?.attributedText = NSMutableAttributedString()
             .regular(string: "탈퇴하면 ", fontSize: 18)
