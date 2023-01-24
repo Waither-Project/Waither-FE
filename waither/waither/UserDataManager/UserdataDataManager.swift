e04650ee16bc44ac49da733817ab99ed5defb84d
김유빈 <yubin6894@gmail.com>
2023-01-24 14:11:24 +0900

    [Docs] add .gitattributes file

---
+++ waither/waither/UserDataManager/UserdataDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserdataDataManager.swift
+//  waither
+//
+//  Created by 김유빈 on 2023/01/24.
+//
+
+import Foundation
---
+++ waither/waither/UserDataManager/UserdataModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserDataModel.swift
+//  waither
+//
+//  Created by 김유빈 on 2023/01/24.
+//
+
+import Foundation
