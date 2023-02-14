aa05b01ab6bcd650e81d76f42f3db1db03b0ce82
김유빈 <yubin6894@gmail.com>
2023-02-15 02:49:22 +0900

    [Feat] import SaveVC name data

--- waither/waither/Survey/SaveViewController.swift
+++ waither/waither/Survey/SaveViewController.swift
@@ -12,10 +12,14 @@ class SaveViewController: UIViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
     
+    var nameData : UserModel!
+    var name : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        label1.text = "OO님의\n데이터가 저장되었어요."
-        label2.text = "이제 웨이더가\nOO님에게 꼭 맞는\n기상예보를 보내드릴게요."
+        
+        SurveyDataManager().nameDataManager(self)
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
         
         saveView.addGestureRecognizer(tapGesture)
@@ -24,23 +28,21 @@ class SaveViewController: UIViewController {
     @objc func handleTap(sender: UITapGestureRecognizer) {
         print("tap")
         let Storyboard = UIStoryboard.init(name: "MainPage", bundle: nil)
-    
+        
         let nextVC = Storyboard.instantiateViewController(withIdentifier: "MainPageVC") as! MainPageViewController
         let navController = UINavigationController(rootViewController: nextVC)
         navController.modalTransitionStyle = .crossDissolve
         navController.modalPresentationStyle = .fullScreen
         self.present(navController, animated:true, completion: nil)
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SaveViewController {
+    func NameSuccessAPI(_ result : UserModel) {
+        self.nameData = result
+        name = nameData.name
+        
+        label1.text = name + "님의\n데이터가 저장되었어요."
+        label2.text = "이제 웨이더가\n" + name + "님에게 꼭 맞는\n기상예보를 보내드릴게요."
     }
-    */
-
 }
---
+++ waither/waither/Survey/SurveyDataManager.swift
@@ -0,0 +1,25 @@
+//
+//  SurveyDataManager.swift
+//  waither
+//
+//  Created by 김유빈 on 2023/02/15.
+//
+
+import Alamofire
+
+class SurveyDataManager {
+    func nameDataManager(_ viewcontroller : SaveViewController) {
+        AF.request("https://www.waither.shop/users/settings/user?userIdx=1",
+                   method: .get,
+                   parameters: nil)
+        .validate()
+        .responseDecodable(of: APIModel<UserModel>.self) { response in
+            switch response.result {
+            case .success(let value):
+                viewcontroller.NameSuccessAPI(value.result!)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
